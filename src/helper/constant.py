ee6711adb01230aaaed5f6e7a8d7c0d76aa8337a
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-06-13 10:53:42 +0700

    Update constant.py

--- src/helper/constant.py
+++ src/helper/constant.py
@@ -8,18 +8,18 @@ Tekan tombol <b>Tutorial</b> dibawah ini untuk melihat bagaimana cara mengubah <
 <i>Jadikan aku sebagai <b>admin</b> dan beri aku hak untuk <b>menghapus pesan.</b></i>
 """
     ABOUT = """
-🤖 <b>Nama :</b> <a href=\"t.me/tebakgambarid_bot\">Tebak Gambar</a>
+🤖 <b>Nama :</b> <a href=\"t.me/riotebakgambarbot\">Tebak Gambar</a>
 
-🧑‍💻 <b>Developer :</b> <a href=\"t.me/tokai\">tokai</a>
+🧑‍💻 <b>Developer :</b> <a href=\"t.me/fckualot\">Rio</a>
 
 📚 <b>Pustaka :</b> <a href=\"https://python-telegram-bot.readthedocs.io/en/stable\">python-telegram-bot</a>
 
 🏷 <b>Bahasa :</b> <a href=\"https://docs.python.org/3\">python 3</a>
 """
     HELP = """
-Ganti sendiri textnya!
+Hai!, Cukup Tambahkan Bot ini ke Group Anda Dan Angkat Menjadi Admin untuk bermain!
 """
     START = """
-Langsung saja yuk, kita bermain... kamu dapat memulainya dengan
+Halo, Saya Adalah Bot Tebak Gambar Yang Dibuat Oleh @fckualot as Rio, Langsung saja yuk, kita bermain... kamu dapat memulainya dengan
 menekan tombol Petunjuk dibawah untuk membaca tentang cara menggunakanku. Enjoy!!!
 """
